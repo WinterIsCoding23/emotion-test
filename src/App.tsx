872f9a8e003bb978faf14524c4532d10e615e7bf
Markus Winter <markusdominicwinter@hotmail.com>
2023-10-01 18:02:02 +0200

    uncomment App.css and add border to h2

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,34 @@
 /** @jsx jsx */
 import React from "react";
 import { jsx, css, SerializedStyles } from "@emotion/react";
-// import "./App.css";
+import "./App.css";
 
 const paraStyle: SerializedStyles = css({
   color: "red",
+  marginTop: 10,
+  marginLeft: 10,
+  marginRight: 10,
 });
 
 function App() {
   return (
     <div className="App">
-      <h2 css={{ color: "green" }}>Title</h2>
-      <p css={paraStyle}>This is some text</p>
+      <h2 css={{ color: "green", textAlign: "center" }}>Title</h2>
+      <p css={paraStyle}>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+        amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+        rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+        ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing
+        elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+        aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo
+        dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
+        est Lorem ipsum dolor sit amet.
+      </p>
     </div>
   );
 }
