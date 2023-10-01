a9457e41ed8044f2b233ac58b4aaa620fe32cdc6
Markus Winter <markusdominicwinter@hotmail.com>
2023-10-01 16:24:40 +0200

    add type SerializedStyles to paraStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import React from "react";
-import { jsx, css } from "@emotion/react";
+import { jsx, css, SerializedStyles } from "@emotion/react";
 // import "./App.css";
 
-const paraStyle = css({
+const paraStyle: SerializedStyles = css({
   color: "red",
 });
 
 function App() {
   return (
     <div className="App">
+      <h2 css={{ color: "green" }}>Title</h2>
       <p css={paraStyle}>This is some text</p>
     </div>
   );
