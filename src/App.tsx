b46d24f990d5edf8df195b28101dd9440eae4522
Markus Winter <markusdominicwinter@hotmail.com>
2023-10-11 17:38:04 +0200

    paste in larger dummy-text & define showButton

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 // import React from "react";
 import { css, SerializedStyles } from "@emotion/react";
+import { useState, useEffect } from "react";
 import StyledButton from "./styledComponents/StyledButton";
 import StyledContainer from "./styledComponents/StyledContainer";
 import "./App.css";
@@ -11,8 +12,10 @@ const paraStyle: SerializedStyles = css({
   marginRight: 10,
 });
 
-
 function App() {
+  const [showButton, setShowButton] = useState(false);
+  
+
   return (
     <div className="App">
       <StyledContainer>
@@ -31,7 +34,38 @@ function App() {
           invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
           At vero eos et accusam et justo duo dolores et ea rebum. Stet clita
           kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-          amet.
+          amet. Duis autem vel eum iriure dolor in hendrerit in vulputate velit
+          esse molestie consequat, vel illum dolore eu feugiat nulla facilisis
+          at vero eros et accumsan et iusto odio dignissim qui blandit praesent
+          luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+          nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+          volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+          ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+          Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse
+          molestie consequat, vel illum dolore eu feugiat nulla facilisis at
+          vero eros et accumsan et iusto odio dignissim qui blandit praesent
+          luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+          Nam liber tempor cum soluta nobis eleifend option congue nihil
+          imperdiet doming id quod mazim placerat facer possim assum. Lorem
+          ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy
+          nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+          Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper
+          suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem
+          vel eum iriure dolor in hendrerit in vulputate velit esse molestie
+          consequat, vel illum dolore eu feugiat nulla facilisis. At vero eos et
+          accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+          no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum
+          dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+          tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+          voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+          Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+          dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing
+          elitr, At accusam aliquyam diam diam dolore dolores duo eirmod eos
+          erat, et nonumy sed tempor et et invidunt justo labore Stet clita ea
+          et gubergren, kasd magna no rebum. sanctus sea sed takimata ut vero
+          voluptua. est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+          consetetur
         </p>
         <StyledButton type="button">Some Button</StyledButton>
       </StyledContainer>
