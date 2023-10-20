ba234e0771c2272ee999c3a8102c79f136100949
Markus Winter <markusdominicwinter@hotmail.com>
2023-10-20 16:13:55 +0200

    write test for paragraph

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,3 +6,9 @@ test("renders heading", () => {
   const heading = screen.getByRole(/heading/i);
   expect(heading).toBeInTheDocument();
 });
+
+test("renders paragraph", () => {
+  render(<App />);
+  const paragraph = screen.getByText(/lorem ipsum/i);
+  expect(paragraph).toBeInTheDocument();
+});
